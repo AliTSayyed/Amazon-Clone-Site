302aa9b701397e773f1c4a8ab7c6421bd37a02ee
alitsayyed <alitsayyed@gmail.com>
2024-07-19 10:48:46 -0500

    Move add to cart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,26 @@
 // when add to cart it pressed, items are added to the cart list as objects containing an ID and a quantity. 
 export const cart = [];
 
+export function addToCart(productId) {
+  // set undefinded var if item already exists in the cart (do not want to add it again to the cart)
+  let matchingItem;
+
+  // check if product is already in the cart array
+  // the cart array variable is from the data/cart.js file
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  // increment the quantity of the item if it is already in the cart
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    // if the product is not in the cart, add it. 
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 // Importing variables without needing to create a script tag in the html. '../' refers to exiting the current file and going to the main folder. 
 // Importing modules helps avoid naming conflicts.
-import { cart } from '../data/cart.js';
+import { cart, addToCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 
 // store the data for each product as objects in a list (list is in the data/products.js file)
@@ -66,43 +66,27 @@ products.forEach((product) => {
 // use the dom to modify the html with the generated product's html
 document.querySelector('.js-products-grid').innerHTML = productsHTMl;
 
+function updateCartQuantity(){
+   // update the amount of items displyed on the cart icon (top right of the page)
+   let cartQuantity = 0;
+   cart.forEach((cartItem) => {
+     cartQuantity += cartItem.quantity;
+   });
+
+   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
 // use the dom to add an event listener when add to cart is pressed 
 // use querySelectorAll since there will be a list of add to cart buttons (for each product)
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      // each time a button is pressed, save the name of the product to the cart
+      // each time a button is pressed, save the ID of the product to the cart
       // use the data- attribute in html to get the product ID for any add to cart button pressed. 
       const productId = button.dataset.productId;
 
-      // set undefinded var if item already exists in the cart (do not want to add it again to the cart)
-      let matchingItem;
-
-      // check if product is already in the cart array
-      // the cart array variable is from the data/cart.js file
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      // increment the quantity of the item if it is already in the cart
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        // if the product is not in the cart, add it. 
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
-
-      // update the amount of items displyed on the cart icon (top right of the page)
-      let cartQuantity = 0;
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+      // call functions to add to cart and update cart quantity
+      addToCart(productId);
+      updateCartQuantity();
     });
   });
