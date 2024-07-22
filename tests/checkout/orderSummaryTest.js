56f99148c8ebd9cfbd0adbe57e5142956279b715
alitsayyed <alitsayyed@gmail.com>
2024-07-22 15:33:44 -0500

    load products in BeforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,11 +1,20 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from '../../data/cart.js';
+import { loadProducts } from "../../data/products.js";
 
 // 1. test how the page looks
 // 2. test how the page behaves 
 describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  // done function makes the hook wait for the inner function to finish before being able to move on to the next lines of code
+  // need to wait for the products to actually load before moving on to next lines of code
+  beforeAll((done)=>{
+    loadProducts(() =>{
+      done(); // waits for loadProducts to finish
+    });
+  });
 
   // before each hook function runs prerequisite code before the actual tests can be done 
   beforeEach(() => {
