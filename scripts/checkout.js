a8d5e1fff5c9ae5daadfd23e48a0440007251300
alitsayyed <alitsayyed@gmail.com>
2024-07-22 08:58:37 -0500

    create class conversion of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import'../data/cart-oop.js'; // runs everything in this file without calling an item to import 
+import'../data/cart-class.js'; // runs everything in this file without calling an item to import 
 
 // run the order summary page
 renderOrderSummary();
