22dc37d47594fc307266c417acc2a632177c4200
alitsayyed <alitsayyed@gmail.com>
2024-07-28 13:54:24 -0500

    added "no orders placed" page for empty orders

--- scripts/orders.js
+++ scripts/orders.js
@@ -12,84 +12,106 @@ loadProductsFetch().then(() => {
 
 // function to display the products on a page 
 function renderOrderspage() {
-  // create the html header for each order in the orders list. 
-  let ordersHTML = ``;
-  orders.forEach((order) => {
-    console.log(order);
-    const dateString = dayjs(order.orderTime).format('MMMM D');
-
-    ordersHTML += `
-    <div class="order-container">
-      <div class="order-header">
-        <div class="order-header-left-section">
-          <div class="order-date">
-            <div class="order-header-label">Order Placed:</div>
-            <div>${dateString}</div>
-          </div>
-          <div class="order-total">
-            <div class="order-header-label">Total:</div>
-            <div>$${formatCurrency(order.totalCostCents)}</div>
-          </div>
-        </div>
-
-        <div class="order-header-right-section">
-          <div class="order-header-label">Order ID:</div>
-          <div>${order.id}</div>
+  // show empty orders if no orders are placed
+  if (orders === null || orders.length === 0) {
+    let emptyOrdersHTML = `
+      <div class="empty-orders-center-wrapper">
+        <div class="empty-orders-center-container">
+          <div class="empty-page-title">No Orders Placed</div>
+          <div><img class="empty-cart-image" src="images/icons/empty-cart-icon.png"></div>
+          <a class="back-to-shop-link" href="amazon.html">
+            <button class="button-primary">Back to Shopping</button>
+          </a>
         </div>
       </div>
-      <div class="order-details-grid">
-        `;
-
-    // create the html body for each product in each order 
-    order.products.forEach((product) => {
-      console.log(product)
-      // get the product information from the backend to display on the page
-      const orderedProduct = getProducts(product.productId);
-      // order's product array has quantity and estimated delivery 
-      const dateString = dayjs(product.estimatedDeliveryTime).format('MMMM D');
+    `;
+    document.querySelector('.js-main').innerHTML = emptyOrdersHTML;
+    document.querySelector('.js-cart-quantity').innerHTML = cart.length;
+  } else { // show orders if orders are placed
+    document.querySelector('.js-main').innerHTML = `
+      <div class="page-title">Your Orders</div>
+      <div class="orders-grid js-order-grid">`;
+  
+    // create the html header for each order in the orders list. 
+    let ordersHTML = ``;
+    orders.forEach((order) => {
+      
+      const dateString = dayjs(order.orderTime).format('MMMM D');
 
       ordersHTML += `
-            <div class="product-image-container">
-              <img src="${orderedProduct.image}">
+      <div class="order-container">
+        <div class="order-header">
+          <div class="order-header-left-section">
+            <div class="order-date">
+              <div class="order-header-label">Order Placed:</div>
+              <div>${dateString}</div>
             </div>
-
-            <div class="product-details">
-              <div class="product-name">
-                ${orderedProduct.name}
-              </div>
-              <div class="product-delivery-date">
-                Arriving on: ${dateString}
-              </div>
-              <div class="product-quantity">
-                Quantity: ${product.quantity}
-              </div>
-              <button class="buy-again-button button-primary">
-                <img class="buy-again-icon" src="images/icons/buy-again.png">
-                <span class="buy-again-message">Buy it again</span>
-              </button>
+            <div class="order-total">
+              <div class="order-header-label">Total:</div>
+              <div>$${formatCurrency(order.totalCostCents)}</div>
             </div>
+          </div>
 
-            <div class="product-actions">
-              <a href="tracking.html">
-                <button class="track-package-button button-secondary">
-                  Track package
+          <div class="order-header-right-section">
+            <div class="order-header-label">Order ID:</div>
+            <div>${order.id}</div>
+          </div>
+        </div>
+        <div class="order-details-grid">
+          `;
+
+      // create the html body for each product in each order 
+      order.products.forEach((product) => {
+       
+        // get the product information from the backend to display on the page
+        const orderedProduct = getProducts(product.productId);
+        // order's product array has quantity and estimated delivery 
+        const dateString = dayjs(product.estimatedDeliveryTime).format('MMMM D');
+
+        ordersHTML += `
+              <div class="product-image-container">
+                <img src="${orderedProduct.image}">
+              </div>
+
+              <div class="product-details">
+                <div class="product-name">
+                  ${orderedProduct.name}
+                </div>
+                <div class="product-delivery-date">
+                  Arriving on: ${dateString}
+                </div>
+                <div class="product-quantity">
+                  Quantity: ${product.quantity}
+                </div>
+                <button class="buy-again-button button-primary">
+                  <img class="buy-again-icon" src="images/icons/buy-again.png">
+                  <span class="buy-again-message">Buy it again</span>
                 </button>
-              </a>
+              </div>
+
+              <div class="product-actions">
+                <a href="tracking.html">
+                  <button class="track-package-button button-secondary">
+                    Track package
+                  </button>
+                </a>
+              </div>
+                `;
+      });
+
+      // add divs to close the order container tag and order grid tag after the final product's html is added 
+      ordersHTML += `
+        </div>
+          </div>
+          </div>
             </div>
-  `;
+              `;
     });
 
-    // add divs to close the order container tag
-    ordersHTML += `
-      </div>
-      </div>
-    </div>
-    `;
-  });
+    // for each order stored in the backend, create the html for the order and the products. 
+    document.querySelector('.js-order-grid').innerHTML = ordersHTML;
 
-  // for each order stored in the backend, create the html for the order and the products. 
-  document.querySelector('.js-order-grid').innerHTML = ordersHTML;
-
-  // update the number of items in the cart after ordering (should be 0)
-  document.querySelector('.js-cart-quantity').innerHTML = cart.length;
+    // update the number of items in the cart after ordering (should be 0)
+    document.querySelector('.js-cart-quantity').innerHTML = cart.length;
+  }
 }
